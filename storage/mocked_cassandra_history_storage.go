1c61daf368e8430b402c5fd9fb25aea51bc96ac8
Vadim <vadim.pysarenko97@gmail.com>
2019-05-16 17:09:29 +0300

    fix mocked storage behavior for get_key_accounts and get_controlled_accounts

--- storage/mocked_cassandra_history_storage.go
+++ storage/mocked_cassandra_history_storage.go
@@ -42,12 +42,20 @@ func (mchs *MockedCassandraHistoryStorage) GetTransaction(args GetTransactionArg
 func (mchs *MockedCassandraHistoryStorage) GetKeyAccounts(args GetKeyAccountsArgs) (GetKeyAccountsResult, error) {
 	result := GetKeyAccountsResult{ AccountNames: make([]string, 0) }
 
+	if args.PublicKey == "" {
+		return result, nil
+	}
+
 	result.AccountNames = append(result.AccountNames, "1", "2", "3")
 	return result, nil
 }
 
 func (mchs *MockedCassandraHistoryStorage) GetControlledAccounts(args GetControlledAccountsArgs) (GetControlledAccountsResult, error) {
 	result := GetControlledAccountsResult{ ControlledAccounts: make([]string, 0) }
+
+	if args.ControllingAccount == "" {
+		return result, nil
+	}
 
 	result.ControlledAccounts = append(result.ControlledAccounts, "4", "5", "6")
 	return result, nil
