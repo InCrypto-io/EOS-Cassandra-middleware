7458ef9cb045367361ad16397574d37944742ca7
Vadim <vadim.pysarenko97@gmail.com>
2019-05-16 16:40:50 +0300

    update storage interface; implement get_actions handler

--- routes/get_actions.go
+++ routes/get_actions.go
@@ -1,28 +1,48 @@
 package routes
 
 import (
+	"EOS-Cassandra-middleware/storage"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 
 func (r *Router) handleGetActions() http.HandlerFunc {
-	type RequestParams struct {
-		AccountName string `json:"account_name"`
-		Pos         *int64 `json:"pos"`
-		Offset      *int64 `json:"offset"`
-		//TODO: additional params
-	}
-
-	type Action struct {
-		GlobalActionSeq interface{} `json:"global_action_seq"`
-		// TODO...
-	}
-	type Response struct {
-		Actions               []Action `json:"actions"`
-		LastIrreversibleBlock   uint64 `json:"last_irreversible_block"`
-	}
-
 	return func(writer http.ResponseWriter, request *http.Request) {
-		// TODO: implement
+		bytes, err := ioutil.ReadAll(request.Body)
+		defer request.Body.Close()
+		if err != nil {
+			writeErrorResponse(writer, http.StatusInternalServerError, "Internal service error")
+			log.Println("Failed to read request body. Error: " + err.Error())
+			return
+		}
+
+		var args storage.GetActionArgs
+		if len(bytes) > 0 {
+			if err = json.Unmarshal(bytes, &args); err != nil {
+				if _, ok := err.(*json.SyntaxError); ok {
+					writeErrorResponse(writer, http.StatusBadRequest, "Invalid json in request body")
+					log.Println("Invalid request body. Error: " + err.Error())
+					return
+				}
+			}
+		}
+
+		response, err := r.historyStorage.GetActions(args)
+		if err != nil {
+			writeErrorResponse(writer, http.StatusInternalServerError, "Internal service error")
+			log.Println("Got error from IHistoryStorage.GetActions(). Error: " + err.Error())
+			return
+		}
+		b, err := json.Marshal(response)
+		if err != nil {
+			writeErrorResponse(writer, http.StatusInternalServerError, "Internal service error")
+			log.Println("Failed to marshal response. Error: " + err.Error())
+			return
+		}
+		fmt.Fprintf(writer, string(b))
 	}
 }
--- storage/storage.go
+++ storage/storage.go
@@ -1,9 +1,55 @@
 package storage
 
+//get_actions
+type GetActionArgs struct {
+	AccountName string `json:"account_name"`
+	Pos         *int64 `json:"pos"`
+	Offset      *int64 `json:"offset"`
+	//TODO: additional params
+}
+
+type Action struct {
+	GlobalActionSeq interface{} `json:"global_action_seq"`
+	// TODO...
+}
+type GetActionsResult struct {
+	Actions               []Action `json:"actions"`
+	LastIrreversibleBlock   uint64 `json:"last_irreversible_block"`
+}
+
+//get_transaction
+type GetTransactionArgs struct {
+	ID string `json:"id"`
+}
+
+type GetTransactionResult struct {
+	ID                      string `json:"id"`
+	// TODO...
+	LastIrreversibleBlock   uint64 `json:"last_irreversible_block"`
+}
+
+//get_key_accounts
+type GetKeyAccountsArgs struct {
+	PublicKey string `json:"public_key"`
+}
+
+type GetKeyAccountsResult struct {
+	AccountNames []string `json:"account_names"`
+}
+
+//get_controlled_accounts
+type GetControlledAccountsArgs struct {
+	ControllingAccount string `json:"controlling_account"`
+}
+
+type GetControlledAccountsResult struct {
+	ControlledAccounts []string `json:"controlled_accounts"`
+}
+
 
 type IHistoryStorage interface {
-	GetActions(/*TODO: arguments*/) /*TODO: response*/
-	GetTransaction(/*TODO: arguments*/) /*TODO: response*/
-	GetKeyAccounts(/*TODO: arguments*/) /*TODO: response*/
-	GetControlledAccounts(/*TODO: arguments*/) /*TODO: response*/
+	GetActions(GetActionArgs)                        (GetActionsResult,            error)
+	GetTransaction(GetTransactionArgs)               (GetTransactionResult,        error)
+	GetKeyAccounts(GetKeyAccountsArgs)               (GetKeyAccountsResult,        error)
+	GetControlledAccounts(GetControlledAccountsArgs) (GetControlledAccountsResult, error)
 }
