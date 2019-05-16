b9725b5697b29c6c8d9fe373b3212a6194329076
Vadim <vadim.pysarenko97@gmail.com>
2019-05-16 12:32:33 +0300

    add main

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"EOS-Cassandra-middleware/routes"
+	"EOS-Cassandra-middleware/storage"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const ConfigFilename = "config.json"
+
+type Config struct {
+	Port uint32 `json:"port"`
+}
+
+
+func main() {
+	var config Config
+	file, err := os.Open(ConfigFilename)
+	if err != nil {
+		log.Printf("Failed to open %s\n", ConfigFilename)
+		return
+	}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Printf("Failed decode config\n")
+		return
+	}
+	file.Close()
+
+	var hs storage.IHistoryStorage //TODO: init
+	router := routes.NewRouter(hs)
+	address := ":" + strconv.FormatUint(uint64(config.Port), 10)
+	s := http.Server{ Addr: address, Handler: router }
+	err = s.ListenAndServe()
+	if err != http.ErrServerClosed {
+		log.Println("s.ListenAndServe() returned error: " + err.Error())
+	}
+}
